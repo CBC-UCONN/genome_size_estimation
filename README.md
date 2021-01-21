1ff168b779c40d469896e6e9a2b5c6dd63f111e3
Noah Reid <noah.reid@uconn.edu>
2021-01-21 14:33:42 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
+When doing genome sequencing projects, something we often want to know _a priori_, but don't, is the approximate size of the genome. We can get an estimate of this from raw, unassembled sequencing data provided the coverage is high enough and the error rate is low enough (you can't use nanopore reads for this!)
 
+There are a few ways to do this. We'll illustrate the basic concept first and how features of real data complicate things, and then move on to using a piece of software called `genomescope`. 
+
+When sequencing a genome, some key numbers are the size  of the haploid genome (let's call that G), the total number of bases sequenced by the researcher (call that Bn) and the sequencing coverage, or the expected number of times each base in the genome has been sequenced (call that Cb). Consider that: 
+
+Cb = Bn/G
+
+We can easily tally up Bn. But is there a way to learn C and thus figure out G without having an assembled genome? Yes, sort of, by using the k-mer frequency spectrum! k-mers are simply sequences of length k. The k-mer frequency spectrum is the frequency (fc) of k-mers with frequency (fk) in a set of sequences. To calculate this from a set of sequences:
+
+    1. Chop up each sequence into every possible subsequence of length k. 
+    2. Calculate the frequency (fk) of each unique k-mer. 
+    3. Calculate the frequency (fc) of the frequencies of k-mers (fk). 
+
+This can be a little confusing because the k-mer frequency spectrum gives the frequency of frequencies of k-mers, but this is key so take a moment to wrap your brain around it. To make it a little more concrete, consider this 16bp sequence and it's 10bp k-mers below:
+
+![](images/kmers.jpg)
+
+To get all 10-mers from that sequence, simply take each substring from bases 1-10,2-11,... This results in 16 - 10 + 1 = 7 k-mers, each one unique. So fk=(1,1,1,1,1,1,1) and fc=(7). So our k-mer "spectrum", in this case is a single pair of numbers, , because each of our 7 kmers occurs only once. 
+
+
+
+So any given sequence of length G has Kn = G - Kl + 1 k-mers of length Kl. For typical focal k-mer lengths of 15-30bp and genomes in the millions to billions of bases, the number of bases in the (haploid) genome, G, is approximately equal to the number of k-mers. 
+
+For a collection of Rn short reads of uniform length Rl, the number of k-mers that can be extracted is Knr = Rn * (Rl - Kl + 1). When dealing with sequencing data, which can generally come from either strand, _canonical_ k-mers are usually considered. For canonical k-mers, sequences and their reverse complements are equivalent. So AGAG and CTCT form a single canonical 4-mer. 
+
+Ok, so once we get this k-mer spectrum (more about that shortly), how do we figure out the length, G, of our genome? First we have to take one more short step. Above we noted that Cb = Bn/G. With our k-mer spectrum we can come up with an analog: Ck = KnS/Kn. The expected frequency (or coverage) of a genomic k-mer in our sequencing data (Ck) equals the total number of k-mers in the sequencing data (KnS) divided by the number of k-mers in the genome. As we noted above, as long as we're working with short k-mer lengths and large genomes, Kn is about equal to the genome length. 
+
+So, finally, let's look at how we learn Ck from the k-mer spectrum. 
+
+
+
+
+    1. calculate kmer frequency spectrum. 
+        - the frequency, f, of kmers with frequency c in the read data
+    2. for a haploid genome
+
+
+```R
+# peak #1
+c1 <- 
+```
---
+++ kmer_sim.R
@@ -0,0 +1,161 @@
+library(tidyverse) 
+
+# The purpose of the script is to demonstrate in principle how
+# distributions of k-mer frequencies can be used to 
+# infer genome size in a really simple way. Actual 
+# empirical data are much messier, so methods implemented
+# in software such as genomescope are preferable. 
+
+# The script works by simulating a random diploid genome 
+# and sequencing reads from that genome. 
+
+
+# first specify genome length (don't pick a realistic eukaryotic number)
+G <- 100000
+
+# In diploid organisms, there are usually differences
+# between homozygous chromosomes. Simulate set the frequency here. 
+
+# mutations:
+	# set an arbitrary translation vector to "mutate" the homologous chromosome
+	mut <- c(A="T",C="G",G="C",T="A")
+	# heterozygosity (fraction of heterozygous sites)
+	hz <- 0.01
+	# expected number of variable sites
+	nvar <- round(hz * G)
+
+
+# create random genome sequence
+seq1 <- sample(c("A","C","G","T"),size=G,replace=TRUE) 
+# create homologous sequence (with heterozygous sites)
+seq2 <- seq1
+	if(nvar > 0){
+		s <- sample(1:G,nvar)
+		seq2[s] <- mut[seq2[s]]
+	}
+
+# concatenate bases into sequences
+seq1 <- paste(seq1, collapse="")
+seq2 <- paste(seq2, collapse="")
+
+# put both sequences into a list object
+seqlist <- list(seq1,seq2)
+
+# now we have a diploid genome with heterozygous sites
+# simulate sequencing data with error + chopping up k-mers
+
+# expected per-base sequencing coverage
+ec <- 60
+
+# read length
+rl <- 100
+
+# number of reads needed to achieve coverage
+rn <- round(G/rl * ec)
+
+# random read start positions:
+rstart <- sample(1:(G-rl+1),rn,replace=TRUE)
+
+# which chromosome should the read come from?
+rchrom <- sample(1:2,rn,replace=TRUE)
+
+# how many errors should the read have?
+	# per read error rate
+	e <- 0.001 * rl
+	# add error to this many reads
+	rerror <- round(e * rn)
+	# place error in these positions:
+	rerrpos <- sample(1:rl,rn,replace=TRUE)
+
+# initialize read vector of length 'rn'
+reseq <- character(rn)
+
+# extract first 'rerror' sequences, add errors
+if(rerror > 0){
+	for(i in 1:rerror){
+		# extract read
+		reseq[[i]] <- substr(
+			seqlist[[rchrom[i]]],
+			rstart[i],
+			(rstart[i] + rl - 1))
+		# add error
+		substr(reseq[[i]],rerrpos[i],rerrpos[i]) <- mut[substr(reseq[[i]],rerrpos[i],rerrpos[i])]
+		if((i %% 1000) == 0){print(i)}
+	}
+}
+
+# extract remaining sequences without errors
+for(i in (rerror+1):rn){
+	# extract read
+	reseq[[i]] <- substr(
+		seqlist[[rchrom[i]]],
+		rstart[i],
+		(rstart[i] + rl - 1))
+	if((i %% 1000) == 0){print(i)}
+}
+
+
+# decompose reads into k-mers
+
+# k-mer length
+kl <- 20
+
+# number of k-mers in a read
+kn <- rl - kl + 1
+
+# initialize kmer vector of length 'kn * rn'
+kmers <- character(kn * rn)
+
+# extract 'kn' k-mers from each of 'rn' reads
+for(i in 1:rn){
+	for(j in 1:kn){
+		k <- (i - 1) * kn + j
+		kmers[k] <- substr(reseq[[i]],j,(j+kl-1))
+	}
+	if((i %% 1000) == 0){print(i)}
+}
+
+# collect frequency of each k-mer (note, "non-canonical" k-mer tally)
+cc <- table(kmers)
+
+# collect frequencies of k-mer frequencies (k-mer spectrum)
+ff <- table(cc) %>% data.frame()
+	ff[,1] <- ff[,1] %>% as.character() %>% as.numeric()
+
+# plot k-mer spectrum
+plot(ff,pch=20,type="b",xlab="k-mer frequency",ylab="frequency of k-mer frequencies")
+
+
+# How can we infer the size of the genome from the k-mer spectrum?
+
+# with no error, no heterozygosity, and an unbiased sequencing 
+# process, we could calculate it as the total number of k-mers
+# divided by the average k-mer frequency:
+
+# using average k-mer frequency:
+Kmean <- sum(ff[,1]*ff[,2])/sum(ff[,2])
+Ktotal <- sum(ff[,1]*ff[,2])
+Ktotal / Kmean + kl - 1
+# this won't be correct if e > 0
+
+# sequencing error complicates things, so we want to try to exclude 
+# those very low frequency k-mers that are probably errors
+# exclude k-mers with frequency < 5
+ex <- 1:4
+
+Kmean <- sum(ff[-ex,1]*ff[-ex,2])/sum(ff[-ex,2])
+Ktotal <- sum(ff[-ex,1]*ff[-ex,2])
+Ktotal / Kmean + kl - 1
+# this won't be correct if hz > 0
+
+# heterozygosity complicates things further. the average
+# k-mer frequency is no longer useful. now our plot has 2
+# peaks. the left-most peak is composed of heterozygous k-mers. 
+# we want to use the mode of the right-most peak. 
+ex <- 1:4
+Kmode <- ff[-ex,][which.max(ff[-ex,2]),1]
+Ktotal <- sum(ff[-ex,1]*ff[-ex,2])
+
+Ktotal/Kmode + kl - 1
+
+
